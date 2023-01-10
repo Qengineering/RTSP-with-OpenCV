4a9a0ee3ec0904056a03442d63148ca54f2785a3
Q-engineering <info@qengineering.eu>
2023-01-10 15:40:31 +0100

    C++ source files

--- src/RTSPcam.cpp
+++ src/RTSPcam.cpp
@@ -14,32 +14,51 @@ RTSPcam::RTSPcam(const std::string& filename, int apiPreference)
     cv::Mat frame;
     std::chrono::steady_clock::time_point Tyet;
 
+    MyFile=filename;
+
+    Picture=(filename.find("jpg")!=std::string::npos || filename.find("png")!=std::string::npos);
+
+    if(Picture){
+        std::cout << "Open picture : " << filename << std::endl;
+        return;
+    }
+
     cap.open(filename, apiPreference);
 
     if(!cap.isOpened()) {
         throw std::runtime_error("Failed to open camera");
     }
-    //measure fps cap.get(cv::CAP_PROP_FPS) doesn't work always
-    //start with some dummy reads
-    for(n=0;n<DUMMY;n++){
-        if(!cap.read(frame)) break;
+    double FPS=cap.get(cv::CAP_PROP_FPS);
+
+    if(FPS > 0.0 && FPS < 51.0){
+        FrameTime=1000.0/FPS;
     }
-    if(n!=DUMMY){
-        throw std::runtime_error("Can't read some frames");
+    else{
+        //measure fps cap.get(cv::CAP_PROP_FPS) doesn't work always
+        //start with some dummy reads
+        for(n=0;n<DUMMY;n++){
+            if(!cap.read(frame)) break;
+        }
+        if(n!=DUMMY){
+            throw std::runtime_error("Can't read some frames");
+        }
+        Tyet   = std::chrono::steady_clock::now();
+        for(n=0;n<COUNT;n++){
+            if(!cap.read(frame)) break;
+        }
+        if(n!=COUNT){
+            throw std::runtime_error("Can't read some frames");
+        }
+        Tgrab = std::chrono::steady_clock::now();
+        double Elapse = std::chrono::duration_cast<std::chrono::milliseconds> (Tgrab - Tyet).count();
+        FrameTime=Elapse/COUNT;
+        if(FrameTime<19.0) FrameTime=19.0; //limit to 52.631 FPS max
+        FPS=1000.0/FrameTime;
     }
-    Tyet   = std::chrono::steady_clock::now();
-    for(n=0;n<COUNT;n++){
-        if(!cap.read(frame)) break;
-    }
-    if(n!=COUNT){
-        throw std::runtime_error("Can't read some frames");
-    }
-    Tgrab = std::chrono::steady_clock::now();
-    double Elapse = std::chrono::duration_cast<std::chrono::milliseconds> (Tgrab - Tyet).count();
-    FrameTime=Elapse/COUNT;
 
     std::cout << "Open camera : " << filename << std::endl;
-    std::cout << "FPS : " << 1000.0/FrameTime << std::endl;
+    std::cout << "FrameTime : " << FrameTime << std::endl;
+    std::cout << "FPS : " << FPS << std::endl;
 }
 //----------------------------------------------------------------------------------------
 RTSPcam::~RTSPcam()
@@ -54,6 +73,12 @@ bool RTSPcam::GetLatestFrame(cv::Mat& frame)
     int LostFrames;
     std::chrono::steady_clock::time_point Tyet;
 
+    if(Picture){
+        frame = cv::imread(MyFile);
+        return !frame.empty();
+    }
+
+    //no static picture but a video
     Tyet   = std::chrono::steady_clock::now();
     Elapse = std::chrono::duration_cast<std::chrono::milliseconds> (Tyet - Tgrab).count();
     if(Elapse > FrameTime){

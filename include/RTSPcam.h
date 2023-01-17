bc4f57cb1a9340caa79db36debb2b462138225db
Q-engineering <info@qengineering.eu>
2023-01-17 18:30:15 +0100

    C++ header file

--- include/RTSPcam.h
+++ include/RTSPcam.h
@@ -6,21 +6,26 @@
 #ifndef RTSPCAM_H
 #define RTSPCAM_H
 
+#include "dirent.h"
 #include <opencv2/opencv.hpp>
-
 
 class RTSPcam
 {
 public:
-    RTSPcam(const std::string& filename, int apiPreference = cv::CAP_ANY);
+    RTSPcam(const std::string& MyString, int apiPreference = cv::CAP_ANY);
     virtual ~RTSPcam();
     bool GetLatestFrame(cv::Mat& frame);
-    bool Picture;       // true when a jpg or png picture is loaded.
+    bool UsePicture;    // true when a jpg, png or bmp picture is loaded.
+    bool UseFolder;     // true when a only folder name is loaded.
     double FrameTime;   // 1/FPS
 protected:
-    cv::VideoCapture cap;
+    cv::VideoCapture* cap;
+    bool FirstPic;
     std::string MyFile;
+    int MyApiPreference;
 private:
+    DIR *dir;
+    struct dirent *ent;
     std::chrono::steady_clock::time_point Tgrab;
 };
 

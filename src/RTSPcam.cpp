363eed19899335fe66e3c7ea737e8aa673daa7f8
Q-engineering <info@qengineering.eu>
2023-01-23 10:54:55 +0100

    C++ source files

--- src/RTSPcam.cpp
+++ src/RTSPcam.cpp
@@ -11,20 +11,33 @@
 
 using namespace std;
 //----------------------------------------------------------------------------------------
-RTSPcam::RTSPcam(const string& MyString, int apiPreference): cap(NULL), FirstPic(true)
+RTSPcam::RTSPcam(void): cap(NULL), FirstPic(true), FrameCnt(0)
 {
-    int n;
-    struct stat s;
-    double Elapse;
-    cv::Mat frame;
-    chrono::steady_clock::time_point Tyet;
-
-    MyFile = MyString;
-    MyApiPreference = apiPreference;
+    cap = new cv::VideoCapture;
     UsePicture = false;
-    UseFolder  = false;
+    UseFolder  = false;                 // true when a only folder name is loaded.
+    CurrentFileName = "";
+}
+//----------------------------------------------------------------------------------------
+RTSPcam::~RTSPcam()
+{
+    cap->release();
+    delete cap;
+}
+//----------------------------------------------------------------------------------------
+void RTSPcam::Open(const int Value)
+{
+    cout << "Connecting to : "<< Value << endl;
 
-    cap = new cv::VideoCapture;   //call always even when you only load one picture
+    cap->open(Value);
+
+    ProcessOpen();
+}
+//----------------------------------------------------------------------------------------
+void RTSPcam::Open(const string& MyString, int apiPreference)
+{
+    struct stat s;
+    MyFile = MyString;
 
     if(stat(MyString.c_str(),&s)==0){
         if(s.st_mode & S_IFREG){
@@ -41,15 +54,25 @@ RTSPcam::RTSPcam(const string& MyString, int apiPreference): cap(NULL), FirstPic
         }
     }
 
-    cout << "connecting to "<< MyString << endl;
+    cout << "Connecting to : "<< MyString << endl;
 
     cap->open(MyString, apiPreference);
+
+    ProcessOpen();
+}
+//----------------------------------------------------------------------------------------
+void RTSPcam::ProcessOpen(void)
+{
+    int n;
+    double Elapse;
+    cv::Mat frame;
+    chrono::steady_clock::time_point Tyet;
 
     if(!cap->isOpened()){
         throw runtime_error("Failed to open camera");
     }
 
-    double FPS=cap->get(cv::CAP_PROP_FPS);
+    FPS=cap->get(cv::CAP_PROP_FPS);
 
     if(FPS > 0.0 && FPS < 51.0){
         FrameTime=1000.0/FPS;
@@ -68,23 +91,16 @@ RTSPcam::RTSPcam(const string& MyString, int apiPreference): cap(NULL), FirstPic
 
         Elapse   = chrono::duration_cast<chrono::milliseconds> (Tgrab - Tyet).count();
         FrameTime=Elapse/COUNT;
-        if(FrameTime<19.0) FrameTime=19.0; //limit to 52.631 FPS max
+        if(FrameTime<19.0) FrameTime=19.0;      //limit to 52.631 FPS max
         FPS=1000.0/FrameTime;
     }
 
-    cout << "Open camera : " << MyString << endl;
     cout << "FPS : " << FPS << endl;
-}
-//----------------------------------------------------------------------------------------
-RTSPcam::~RTSPcam()
-{
-    cap->release();
-    delete cap;
 }
 //----------------------------------------------------------------------------------------
 bool RTSPcam::GetLatestFrame(cv::Mat& frame)
 {
-    bool Success;
+    bool Success=false;
     double Elapse;
     int LostFrames;
     string FullName;
@@ -93,7 +109,11 @@ bool RTSPcam::GetLatestFrame(cv::Mat& frame)
 
     if(UsePicture){
         frame = cv::imread(MyFile);
-        return !frame.empty();cout << "delete...\n" ;
+        if(!frame.empty()){
+            NiceString(MyFile);
+            Success=true;
+        }
+        return Success;
     }
     if(UseFolder){
         //open folder
@@ -113,23 +133,24 @@ bool RTSPcam::GetLatestFrame(cv::Mat& frame)
 
                 frame = cv::imread(FullName);
                 if(!frame.empty()){
+                    NiceString(FullName);
                     Success=true;
-                    break;cout << "delete...\n" ;
+                    break;
                 }
             }
         }
         return Success;
     }
 
-
     //no static picture but a video
     if(!FirstPic){
         Tyet   = chrono::steady_clock::now();
         Elapse = chrono::duration_cast<chrono::milliseconds> (Tyet - Tgrab).count();
         if(Elapse > FrameTime){
-            LostFrames=ceil(1.5*Elapse/FrameTime);      //1.5 to be sure you have always the latest frame, in case FrameTime isn't accurate
+            LostFrames=ceil(1.15*Elapse/FrameTime);     //1.15 to be sure you have always the latest frame, in case FrameTime isn't accurate
             if(LostFrames>500) LostFrames=500;          //don't wait forever.
             //flush the lost frames
+            FrameCnt+=LostFrames;
             for(int n=0;n<LostFrames;n++){
                 if(!cap->read(frame)) break;
             }
@@ -138,28 +159,34 @@ bool RTSPcam::GetLatestFrame(cv::Mat& frame)
     Success = cap->read(frame);
     Tgrab   = chrono::steady_clock::now();
 
-    if(!Success){
-        //lost frame -> reconnect
-        cap->release();
-        delete cap;
-        cap=NULL;
-
-        cout << "try to reconnect...\n" ;
-
-        cap = new cv::VideoCapture;
-        cap->open(MyFile, MyApiPreference);
-        if(!cap->isOpened()){
-            throw runtime_error("Failed to open camera");
-        }
-        Success = cap->read(frame);
-        Tgrab   = chrono::steady_clock::now();
-
-        if(Success) cout << "reconnected...\n" ;
-        else        cout << "no frame received...\n" ;
-    }
-
     FirstPic= false;
+    FrameCnt++;
+    NiceString(FrameCnt);
 
     return Success;
+}
+//----------------------------------------------------------------------------------------
+void RTSPcam::NiceString(const std::string Str)
+{
+    size_t lastindex;
+
+    lastindex = Str.find_last_of("\\/");
+    if (std::string::npos != lastindex){
+        CurrentFileName = Str.substr(lastindex+1, Str.length());
+    }
+
+    lastindex = CurrentFileName.find_last_of(".");
+    if (std::string::npos != lastindex){
+        CurrentFileName = CurrentFileName.substr(0, lastindex);
+    }
+}
+//----------------------------------------------------------------------------------------
+void RTSPcam::NiceString(const size_t Cnt)
+{
+    std::stringstream ss;
+
+    ss << "Frm_" << Cnt;
+
+    CurrentFileName = ss.str();
 }
 //----------------------------------------------------------------------------------------
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <opencv2/opencv.hpp>
 #include "RTSPcam.h"
 #include <chrono>
-#include <thread>
 
 using namespace std;
 
@@ -9,12 +8,14 @@ using namespace std;
 int main()
 {
     cv::Mat frame;
+    RTSPcam cam;
 
     cv::namedWindow("Camera", cv::WINDOW_AUTOSIZE);
 
-    RTSPcam cam("rtsp://192.168.178.129:8554/test/"); //you can dump anything OpenCV eats. (cv::CAP_ANY) BTW,OpenCV first tries FFmpeg
-//    RTSPcam cam("/home/pi/Pictures/plant.jpg"); //you can dump anything OpenCV eats. (cv::CAP_ANY) BTW,OpenCV first tries FFmpeg
-//    RTSPcam cam("/home/pi/Pictures/Storm"); //you can dump anything OpenCV eats. (cv::CAP_ANY) BTW,OpenCV first tries FFmpeg
+//    cam.Open(0); //you can dump anything OpenCV eats. (cv::CAP_ANY) BTW,OpenCV first tries FFmpeg
+    cam.Open("rtsp://192.168.178.129:8554/test/"); //you can dump anything OpenCV eats. (cv::CAP_ANY) BTW,OpenCV first tries FFmpeg
+//    cam.Open("/home/pi/Pictures/plant.jpg"); //you can dump anything OpenCV eats. (cv::CAP_ANY) BTW,OpenCV first tries FFmpeg
+//    cam.Open("/home/pi/Pictures/Storm"); //you can dump anything OpenCV eats. (cv::CAP_ANY) BTW,OpenCV first tries FFmpeg
 
     while(true)
     {
@@ -24,9 +25,10 @@ int main()
         }
         ///place here your time consuming algorithms
 
+//        cout << cam.CurrentFileName << endl;
+
         //show frame
         cv::imshow("Camera",frame);
-
         char esc = cv::waitKey(100);
         if(esc == 27) break;
     }
@@ -35,154 +37,3 @@ int main()
 
     return 0;
 }
-
-/*
-
-
-void wait(void)
-{
-    double A=1.001;
-
-    #define T 5000
-
-    cout << "A " << A << "\n" ;
-
-    for(int i=0;i<T;i++){
-        for(int j=0;j<T;j++){
-            A+=log(A);
-        }
-    }
-
-    cout << "A " << A << "\n" ;
-}
-
-int main()
-{
-    int Cnt=0;
-    cv::Mat frame;
-    cv::VideoCapture *cap;
-
-    cv::namedWindow("Camera", cv::WINDOW_AUTOSIZE);
-
-//    for(int z=0;z<30;z++){
-        cout << "Open...\n" ;
-
-//        cap = new cv::VideoCapture;
-        cap->open("rtsp://192.168.178.108:8554/test/");
-
-        if(cap->isOpened()){
-            while(++Cnt<300){
-                if(cap->read(frame)){
-                    if(!frame.empty()){
-                        //show frame
-                        cv::imshow("Camera",frame);
-
-                        char esc = cv::waitKey(5);
-                        if(esc == 27) break;
-                    }
-                }
-            }
-            cap->release();
-        }
-
-        cout << "delete...\n" ;
-        delete cap;
-
-//    }
-
-
-        cout << "Open...\n" ;
-
-        cap = new cv::VideoCapture;
-        cap->open("rtsp://192.168.178.108:8554/test/");
-
-        if(cap->isOpened()){
-            while(++Cnt<600){
-                if(cap->read(frame)){
-                    if(!frame.empty()){
-                        //show frame
-                        cv::imshow("Camera",frame);
-
-                        char esc = cv::waitKey(5);
-                        if(esc == 27) break;
-                    }
-                }
-            }
-            cap->release();
-        }
-
-        cout << "delete...\n" ;
-        delete cap;
-
-
-    return 0;
-}
-
-
-int main()
-{
-    int MissedFrames;
-    cv::Mat frame;
-
-    cv::namedWindow("Camera", cv::WINDOW_AUTOSIZE);
-
-    MissedFrames=0;
-
-    RTSPcam cam("rtsp://192.168.178.108:8554/test/"); //you can dump anything OpenCV eats. (cv::CAP_ANY) BTW,OpenCV first tries FFmpeg
-//    RTSPcam cam("/home/pi/Pictures/plant.jpg"); //you can dump anything OpenCV eats. (cv::CAP_ANY) BTW,OpenCV first tries FFmpeg
-//    RTSPcam cam("/home/pi/Pictures/Storm"); //you can dump anything OpenCV eats. (cv::CAP_ANY) BTW,OpenCV first tries FFmpeg
-
-    std::cout << "Hit ESC to exit" << "\n" ;
-    while(true)
-    {
-        if(!cam.GetLatestFrame(frame)){
-            //don't quit the app just after one missing frame
-            MissedFrames++;
-            std::cout << "Missed : " << MissedFrames << std::endl;
-
-            std::cout << "wait 2 Sec" << std::endl;
-            wait();
-            std::cout << "reopen the stream" << std::endl;
-            cam.cap.open("rtsp://192.168.178.108:8554/test/");
-
-
-            if(cam.cap.isOpened()){
-                std::cout << "Camera open again" << std::endl;
-                if(!cam.cap.read(frame)){
-                    std::cout << "still in error " << std::endl;
-                }
-                else{
-                    cam.Tgrab = std::chrono::steady_clock::now();
-                    std::cout << "we gaan door... " << std::endl;
-                }
-            }
-            else{
-                std::cout << "Camera gaat niet open. stoppen maar" << std::endl;
-                MissedFrames=100;
-            }
-
-            if(MissedFrames>20){
-                //missed 20 frames in a row. The stream has probably stopped.
-                std::cout << "Missed : " << MissedFrames << std::endl;
-                std::cout << "Capture read error" << std::endl;
-                break;
-            }
-        }
-//        else{
-            MissedFrames=0;
-            ///place here your time consuming algorithms
-
-            //show frame
-            cv::imshow("Camera",frame);
-
-            char esc = cv::waitKey(100);
-            if(esc == 27) break;
-//        }
-    }
-
-    cv::destroyAllWindows() ;
-
-    return 0;
-}
-
-*/
